858ee938a4776560cd8b06bbe7d8fa04f5a16b41
Alexander Gottwald <Alexander.Gottwald@gmail.com>
2013-03-10 15:19:15 +0100

    Lowered requirements for improvetool to .NET 2.0

--- ImproveTool/MainForm.cs
+++ ImproveTool/MainForm.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.IO;
 using System.Xml;
 using System.Xml.XPath;
